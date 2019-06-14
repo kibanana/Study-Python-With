4e40ca7bc91fbc26afea0a9def102352b7d67f65
kyw017763 <kyw017763@gmail.com>
2019-06-14 22:38:32 +0900

    ch01

---
+++ ch01/test.py
@@ -0,0 +1,30 @@
+print ("Hello World!")
+
+print()
+
+a = 10
+b = 20
+
+print(a+b)
+
+if (a == 10):
+    print ("a는 10입니다")
+
+print()
+
+for a in [6, 3]:
+    print(a)
+
+print()
+
+i = 0
+while i < 3:
+    i=i+1
+    print(i)
+
+print()
+
+def sum(a,b):
+    return (a+b)
+
+print(sum(15, 20))
