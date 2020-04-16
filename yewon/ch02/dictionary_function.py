495b20f36b6e37c34dff24f4de9c43dc253b45b7
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-04-17 04:31:05 +0900

    스터디 2일차 by yewon

--- yewon/ch02/dictionary_function.py
+++ yewon/ch02/dictionary_function.py
@@ -6,35 +6,37 @@ d = {
 
 # keys
 print(d.keys())
-
 for k in d.keys():
     print(k)
-
 list_k = list(d.keys()) # d의 keys 객체를 리스트로 변환
+print(list_k)
 
 # values
 print(d.values())
-
 for v in d.values():
     print(v)
-
 list_v = list(d.values()) # d의 values 객체를 리스트로 변환
+print(list_v)
 
 # items
 # (key, item) 형태의 튜플 리스트 반환
 print(d.items())
-
-# clear
-# print(d.clear()) # {}
+print(list(d.items()))
+print(list(d.items())[0])
 
 # get
 print(d.get('birth'))
 # 존재하지 않는 키로 가져올 때 
 # None을 리턴
 # d['notExist'] 는 키오류 발생
-print(d.get('notExist', 'birth')) # get default 값 지정
+print(d.get('birth', 'notExist')) # get default 값 지정
+print(d.get('friends', 'notExist'))
 
 # in
 # 키값으로 검색
 # True, False 반환
 print('name' in d)
+
+# clear
+print(d.clear()) # {}
+print(d)
---
+++ yewon/ch02/set.py
@@ -0,0 +1,42 @@
+# 중복을 허용하지 않고, 순서가 없다.
+# (function 수가 좀 적어서 그냥 한 파일에 모두 연습함)
+
+prac_set1 = set([1,2,4,2,3])
+print(prac_set1)
+
+prac_set2 = set('applepie')
+print(prac_set2)
+
+prac_set3 = set([1, 2, 3, 4, 5])
+prac_set4 = set([5, 6, 7, 8, 9])
+
+# 교집합
+print(prac_set3 & prac_set4)
+print(prac_set3.intersection(prac_set4))
+print(list(prac_set3 & prac_set4))
+
+print()
+
+# 합집합
+print(prac_set3 | prac_set4)
+print(prac_set3.union(prac_set4))
+print(list(prac_set3 | prac_set4))
+
+print()
+
+# 차집합
+print(prac_set3 - prac_set4)
+print(prac_set3.difference(prac_set4))
+print(prac_set4.difference(prac_set3))
+print(prac_set4.difference(prac_set4))
+print(list(prac_set3 - prac_set4))
+
+print()
+
+# add, update, remove
+prac_set3.add(63)
+print(prac_set3)
+prac_set3.update([63, 36, 33, 66]) # add이되 여러 값 add
+print(prac_set3)
+prac_set3.remove(63)
+print(prac_set3)
