fca10e81580f6490851edbe5857e6b4eacdace2a
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-05-22 17:44:25 +0900

    스터디 8일차 문제 답 함수로 바꿈 by yewon (일정때문에 미리)

--- yewon/ch06_1_problem.py
+++ yewon/ch06_1_problem.py
@@ -1,15 +1,16 @@
+def compress_str(s):
+  result = ""
+  curr = ""
+  cnt = 0
+  for i in s:
+    if curr != i:
+      if curr != "":
+        result += curr + str(cnt)
+      curr = i
+      cnt = 1
+    else:
+      cnt += 1
+  result += curr + str(cnt) # 마지막 것이 출력 안되므로
+
 str_input = "aaabbcccccca"
-
-curr = ""
-cnt = 0
-
-for i in str_input:
-  if curr != i:
-    if curr != "":
-      print(curr + str(cnt), end="")
-    curr = i
-    cnt = 1
-  else:
-    cnt += 1
-
-print(curr + str(cnt), end="") # 마지막 것이 출력 안되므로
+print(compress_str(str_input))
--- yewon/ch06_3_problem.py
+++ yewon/ch06_3_problem.py
@@ -1,14 +1,16 @@
 # 글자와 글자 사이는 공백 1개, 단어와 단어 사이는 공백 2개로 구분한다.
 
-# list[0] -> A
-rule = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+def encrypt(s):
+  rule = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+  result = ""
+  encryption_list = list(map(str, s.split(" "))) # split()에 인수 안넣고 실행하면 모든 공백이 사라진다.
+  for i in encryption_list:
+    if i == "":
+      result += " "
+    else:
+      idx = rule.index(i)
+      result += chr(65 + idx)
+  return result
 
 encryption = ".... .  ... .-.. . . .--. ...  . .- .-. .-.. -.--"
-encryption_list = list(map(str, encryption.split(" "))) # split()에 인수 안넣고 실행하면 모든 공백이 사라진다.
-
-for i in encryption_list:
-  if i == "":
-    print(" ", end="")
-  else:
-    idx = rule.index(i)
-    print(chr(65 + idx), end="")
+print(encrypt(encryption))
