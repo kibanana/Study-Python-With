6a4a486288463844b68cc841db2955c5c2e73146
김예원 <yewon@gim-yewon-ui-MacBookPro.local>
2020-07-29 03:37:59 +0900

    스터디 10일차 by yewon

--- yewon/ch07/example1.py
+++ yewon/ch07/example1.py
@@ -2,22 +2,23 @@
 
 import re
 
+# 주민등록번호 뒷자리를 *로 바꾸자
 data = """
 park 800905-1049118
 kim 700905-1059119
 """
 
-# 1)
+# 1) 정규표현식을 모를 때
 result = []
 for line in data.split("\n"):
   word_result = []
   for word in line.split():
-    if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit():
+    if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit(): # 이름인가 / 주민등록번호인가
       word = "%s-*******" % word[:6]
     word_result.append(word) # 이름 -> 변환된 주민등록번호 순서
   result.append(" ".join(word_result))
 print("\n".join(result))
 
-# 2)
+# 2) 정규표현식을 사용할 수 있을 때
 reg = re.compile("(\d{6})[-]\d{7}")
 print(reg.sub("\g<1>-*******", "", data))
